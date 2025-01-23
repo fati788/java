9d872adb9c738c1dcaabd9848970bc9892faf297
fati788 <fela102@g.educaand.es>
2025-01-23 21:52:47 +0100

    ejercicios Poo tema5

---
+++ Programacion/Practica1OPP/TestJuego.java
@@ -0,0 +1,11 @@
+package Programacion.Practica1OPP;
+
+public class TestJuego {
+    public static void main(String[] args) {
+
+        Jugador jugador= new Jugador("fati" ,Jugador.Clase.BRUJO);
+
+        jugador.setArmaDerecha("Arma Derecha" ,Arma.Tipo.ESPADA , 10 , true );
+
+    }
+}
