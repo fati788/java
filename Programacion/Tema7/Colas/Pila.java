44e626d40636d09717881c27af106d1809d94677
fati788 <fela102@g.educaand.es>
2025-03-19 19:00:58 +0100

    ejemplos tema7

---
+++ Programacion/Tema7/Colas/Pila.java
@@ -0,0 +1,21 @@
+package Programacion.Tema7.Colas;
+
+import java.util.LinkedList;
+
+public class Pila<E> {
+    LinkedList<E>procesos;
+
+    public Pila() {
+        this.procesos = new LinkedList<>();
+    }
+    public void insertarProceso(E elemento) {
+         this.procesos.push(elemento);
+    }
+    public E sacarProceso() {
+        return this.procesos.pop();
+    }
+    public E mostrarProceso() {
+        return this.procesos.peek();
+    }
+
+}
