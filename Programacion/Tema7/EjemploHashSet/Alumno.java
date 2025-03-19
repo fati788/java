564f3900c3a31c48032680e1f8a474a1318fccf1
fati788 <fela102@g.educaand.es>
2025-03-19 20:03:23 +0100

    ejemplos tema7

---
+++ Programacion/Tema7/EjemploHashSet/Alumno.java
@@ -0,0 +1,94 @@
+package Programacion.Tema7.EjemploHashSet;
+
+import java.util.Objects;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private Integer edad;
+    private String ciudad;
+    private String dni;
+
+    public Alumno(String nombre, String apellido, String email, Integer edad, String ciudad, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.edad = edad;
+        this.ciudad = ciudad;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Alumno{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", apellido='").append(apellido).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", edad=").append(edad);
+        sb.append(", ciudad='").append(ciudad).append('\'');
+        sb.append(", dni='").append(dni).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(dni, alumno.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(dni);
+    }
+}
